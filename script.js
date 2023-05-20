bdaf95ec9b4b836349f4b0b6c07a0f5e5b94d102
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-20 23:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,17 @@
 function palindrome(str){
 
 }
+var s =	str.toLowerCase()
+var i = 0
+	var j = s.length - 1;
+	while (i<j) {
+		if(s.charAt(i)===s.charAt(j)) {
+			i++;
+			j--;
+		}
+		else {
+			return false
+		}
+		
+	}
 module.exports = palindrome
